758ecec2b74d3e1def9267d96fc7a0b4a858839a
Rabet Bishwokarma <137963826+rabetbishwokarma@users.noreply.github.com>
2023-08-19 16:21:22 -0400

    Update models.py

--- trip_app_1/models.py
+++ trip_app_1/models.py
@@ -7,7 +7,7 @@ class Trip(models.Model):
     duration = models.CharField(max_length=50)
     check_in = models.DateTimeField()
     check_out = models.DateTimeField()
-    # ... other fields
+
 
     def __str__(self):
         return self.country
@@ -15,7 +15,7 @@ class Trip(models.Model):
 class HotelExperience(models.Model):
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
     experience_details = models.TextField()
-    # ... other fields
+
 
     def __str__(self):
         return str(self.trip)
