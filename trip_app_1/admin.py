dd8a4114c7bf51eb35d3e32f1784dcaa36f58820
Rabet Bishwokarma <137963826+rabetbishwokarma@users.noreply.github.com>
2023-08-19 16:21:52 -0400

    Update admin.py

--- trip_app_1/admin.py
+++ trip_app_1/admin.py
@@ -6,9 +6,8 @@ from .models import Trip, HotelExperience
 @admin.register(Trip)
 class TripAdmin(admin.ModelAdmin):
     list_display = ('country', 'tour_area', 'duration', 'check_in', 'check_out')
-    # Add more customization options as needed
 
 @admin.register(HotelExperience)
 class HotelExperienceAdmin(admin.ModelAdmin):
     list_display = ('trip', 'experience_details')
-    # Add more customization options as needed
+
